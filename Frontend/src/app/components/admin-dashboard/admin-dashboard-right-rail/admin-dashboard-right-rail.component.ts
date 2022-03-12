23c0e2ddc9ce73b4c86fd9c82d100eb8dff33348
Ashutosh D. Kedar <ashutoshdkedar@gmail.com>
2022-03-12 11:16:35 +0530

    Deploying changes related frontend-backend connection.

--- Frontend/src/app/components/admin-dashboard/admin-dashboard-right-rail/admin-dashboard-right-rail.component.ts
+++ Frontend/src/app/components/admin-dashboard/admin-dashboard-right-rail/admin-dashboard-right-rail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-admin-dashboard-right-rail',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardRightRailComponent implements OnInit {
 
-  constructor() { }
+  URL: string = "http://localhost:5000/api/v1/User/GetAllUser";
+  headerOptions: object = {
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im5pdGluZ29kYXNlIiwibmJmIjoxNjQ3MDYxNzE1LCJleHAiOjE2NDcwNjUzMTUsImlhdCI6MTY0NzA2MTcxNX0.IMHE8OnrEfOW7CiMPiLnCILFhZUlPQPYe8V6KXxywJI'
+  };
+  users: any = [];
+
+  constructor(private HttpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.getAllUsers();
+  }
+
+  getAllUsers() {
+    this.HttpClient.get(this.URL, this.headerOptions).subscribe(resposeData => {
+      this.users = resposeData;
+    });
   }
 
 }
