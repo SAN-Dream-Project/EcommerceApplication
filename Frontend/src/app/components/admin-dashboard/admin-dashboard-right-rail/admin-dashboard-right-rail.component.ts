03677a4b3c4a2bd5977f231319521ed9a6f5502c
Ashutosh D. Kedar <ashutoshdkedar@gmail.com>
2022-03-12 11:29:58 +0530

    Deploying changes related Bearer Token.

--- Frontend/src/app/components/admin-dashboard/admin-dashboard-right-rail/admin-dashboard-right-rail.component.ts
+++ Frontend/src/app/components/admin-dashboard/admin-dashboard-right-rail/admin-dashboard-right-rail.component.ts
@@ -10,7 +10,7 @@ export class AdminDashboardRightRailComponent implements OnInit {
 
   URL: string = "http://localhost:5000/api/v1/User/GetAllUser";
   headerOptions: object = {
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im5pdGluZ29kYXNlIiwibmJmIjoxNjQ3MDYxNzE1LCJleHAiOjE2NDcwNjUzMTUsImlhdCI6MTY0NzA2MTcxNX0.IMHE8OnrEfOW7CiMPiLnCILFhZUlPQPYe8V6KXxywJI'
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im5pdGluZ29kYXNlIiwibmJmIjoxNjQ3MDY0NjcwLCJleHAiOjE2NDcwNjgyNzAsImlhdCI6MTY0NzA2NDY3MH0.-BN_QVjYcJleIZA-5YppVtzneRaEyNvZ6twlIsSp49k'
   };
   users: any = [];
 
