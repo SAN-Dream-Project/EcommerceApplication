00324cfea30d5208b41c6288f512e596923e8d3f
Ashutosh D. Kedar <ashutoshdkedar@gmail.com>
2022-03-14 13:54:16 +0530

    Deploying changes related to Frontend.

--- Frontend/src/app/admin-dashboard/admin-dashboard/user-section/user-section.component.ts
+++ Frontend/src/app/admin-dashboard/admin-dashboard/user-section/user-section.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
-import {throwError} from "rxjs";
+import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
+import { throwError } from "rxjs";
 
 @Component({
   selector: 'app-user-section',
@@ -10,14 +10,16 @@ import {throwError} from "rxjs";
 export class UserSectionComponent implements OnInit {
 
   URL: string = "https://localhost:5001/api/v1/User/GetAllUser";
-  /*httpOptions: object = new HttpHeaders({ 'Content-Type': 'application/json', Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im5pdGluZ29kYXNlIiwibmJmIjoxNjQ3MjMyODcwLCJleHAiOjE2NDcyMzY0NzAsImlhdCI6MTY0NzIzMjg3MH0.fo6uUO1BwJ-EIE_ejH_XLIgsf5DqhYt4FH4lm9VJ798'});
-  httpOptions: object = {
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im5pdGluZ29kYXNlIiwibmJmIjoxNjQ3MjMyODcwLCJleHAiOjE2NDcyMzY0NzAsImlhdCI6MTY0NzIzMjg3MH0.fo6uUO1BwJ-EIE_ejH_XLIgsf5DqhYt4FH4lm9VJ798'
-  };*/
+  UTL_TOGET_BEARER_TOKEN: any = "https://localhost:5001/api/v1/TokenAuthentication/authentication";
+  userCredentials:object = {
+    userName: "nitingodase",
+    password: "123qwe"
+  };
+  bearerToken: any = '';
   httpOptions: object = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im5pdGluZ29kYXNlIiwibmJmIjoxNjQ3MjM0MjU2LCJleHAiOjE2NDcyMzc4NTYsImlhdCI6MTY0NzIzNDI1Nn0.SSZAJP6_g15ykwlcre5nD41HZjmr7X7bfVeb94kth5g'
+      Authorization: ''
     })
   };
   users: any = [];
@@ -26,32 +28,51 @@ export class UserSectionComponent implements OnInit {
   constructor(private HttpClient: HttpClient) { }
 
   ngOnInit(): void {
-    this.getAllUsers();
+    /*Promise.resolve(() => {
+      return this.getBearerToken();
+    }).
+    then(() => {
+      return this.getAllUsers();
+    });*/
+    this.getBearerToken();
+    setTimeout(() => {
+      this.getAllUsers();
+    }, 1000);
   }
-
+  getBearerToken() {
+    return this.HttpClient.post(this.UTL_TOGET_BEARER_TOKEN, this.userCredentials).subscribe((token) => {
+      //console.log(token)
+      //this.bearerToken = token;
+    }, (error) => {
+      //console.log(error);
+      this.bearerToken = this.handleError(error);
+    });
+  }
   getAllUsers() {
-    this.HttpClient.get(this.URL, this.httpOptions).subscribe(responseData => {
+    this.HttpClient.get(this.URL, { headers: new HttpHeaders().set('Authorization', 'Bearer '+this.bearerToken) }).subscribe((responseData) => {
       console.table(responseData);
       this.users = responseData;
-    }, error => {
+    }, (error) => {
       console.log(error);
       this.error = this.handleError(error);
     });
   }
 
   private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
+    console.log(error.error.text);
+    return error.error.text;
+    /*if (error.status === 0) {
       // A client-side or network error occurred. Handle it accordingly.
-      return 'An error occurred:'+ error.error;
+      return 'An error occurred:'+ error.error.text;
     } else if (error.status === 401){
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong.
-      return `You are unauthorized :`+ error.error;
+      return `You are unauthorized :`+ error.error.text;
     }
     else {
       // Return an observable with a user-facing error message.
-      return throwError(() => new Error('Something bad happened; please try again later.'));
-    }
+      return error.error.text//throwError(() => new Error('Something bad happened; please try again later.'));
+    }*/
   }
 
 }
