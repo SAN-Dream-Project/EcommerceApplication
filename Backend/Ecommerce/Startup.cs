16f7b405e2c2cb0fc9592963977cf0a2e37b2148
nitin <nitinsgodase@gmail.com>
2022-03-14 09:56:12 +0530

    commit for new build

--- Backend/Ecommerce/Startup.cs
+++ Backend/Ecommerce/Startup.cs
@@ -22,97 +22,96 @@ using System.Text;
 
 namespace Ecommerce.Host
 {
-   public class Startup
-   {
-      private readonly IConfiguration _configuration;
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
 
-      public Startup(IConfiguration configuration)
-      {
-         _configuration = configuration;
-      }
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
 
-      // This method gets called by the runtime. Use this method to add services to the container.  
-      public void ConfigureServices(IServiceCollection services)
-      {
-         services.AddDbContext<EcommerceContext>(options => options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
-         services.AddControllers();
-         services.AddHttpClient();
-         // Automapper Configuration
-         // Automapper Configuration
-         var key = "This is my first Test Key";
-         AutoMapperConfiguration.Configure(); services.AddAuthentication(x =>
-         {
-            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-         }).AddJwtBearer(x =>
-         {
-            x.RequireHttpsMetadata = false;
-            x.SaveToken = true;
-            x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        // This method gets called by the runtime. Use this method to add services to the container.  
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<EcommerceContext>(options => options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers();
+            services.AddHttpClient();
+            // Automapper Configuration
+            // Automapper Configuration
+            var key = "This is my first Test Key";
+            AutoMapperConfiguration.Configure(); services.AddAuthentication(x =>
             {
-               ValidateIssuerSigningKey = true,
-               ValidateIssuer = false,
-               ValidateAudience = false,
-               IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key))
-            };
-         });
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key))
+                };
+            });
 
-         services.AddSingleton<IJwtAuth>(new Auth(key));
-         services.AddTransient<IRoleRepository, RoleRepository>();
-         services.AddTransient<IRoleAppService, RoleAppService>();
-         services.AddTransient<IUserRepository, UserRepository>();
-         services.AddTransient<IUserAppService, UserAppService>();
-         services.AddControllers();
-         services.AddSwaggerGen(c =>
-         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "EcommerceAPI", Version = "v1" });
-         });
-         services.AddCors();
-      }
+            services.AddSingleton<IJwtAuth>(new Auth(key));
+            services.AddTransient<IRoleRepository, RoleRepository>();
+            services.AddTransient<IRoleAppService, RoleAppService>();
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IUserAppService, UserAppService>();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "EcommerceAPI", Version = "v1" });
+            });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Policy11",
+                builder => builder.AllowAnyOrigin());
+            });
+        }
 
-      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.  
-      public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
-      {
-         var path = Directory.GetCurrentDirectory();
-         loggerFactory.AddFile($"{path}\\Logs\\Log.txt");
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.  
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            var path = Directory.GetCurrentDirectory();
+            loggerFactory.AddFile($"{path}\\Logs\\Log.txt");
 
-         if (env.IsDevelopment())
-         {
-            app.UseDeveloperExceptionPage();
-         }
-         else
-         {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.  
-            app.UseHsts();
-         }
-         if (env.IsDevelopment())
-         {
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EcommerceAPI v1"));
-         }
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.  
+                app.UseHsts();
+            }
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EcommerceAPI v1"));
+            }
 
-         app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
-         app.UseRouting();
+            app.UseRouting();
 
-         app.UseAuthentication();
+            app.UseAuthentication();
 
-         app.UseAuthorization();
+            app.UseAuthorization();
 
-         app.UseEndpoints(endpoints =>
-         {
-            endpoints.MapControllers();
-         });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
 
-         // global cors policy
-         app.UseCors(cors => cors
-             .AllowAnyMethod()
-             .AllowAnyHeader()
-             .SetIsOriginAllowed(origin => true) // allow any origin
-             .AllowCredentials()); // allow credentials
-      }
-   }
+            app.UseCors();
+        }
+    }
 }
