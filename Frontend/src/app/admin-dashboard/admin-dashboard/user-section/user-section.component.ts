e8cd834981d920ea43359927f5ee66a1c5b1d39b
Ashutosh D. Kedar <ashutoshdkedar@gmail.com>
2022-03-15 17:04:44 +0530

    Deploying changes related to Frontend - Binding data fetched from server to table.

--- Frontend/src/app/admin-dashboard/admin-dashboard/user-section/user-section.component.ts
+++ Frontend/src/app/admin-dashboard/admin-dashboard/user-section/user-section.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
-import { throwError } from "rxjs";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { throwError} from "rxjs";
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from 'ngx-spinner';
+import {UsersService} from "./users.service";
 
 @Component({
   selector: 'app-user-section',
@@ -11,23 +12,14 @@ import { NgxSpinnerService } from 'ngx-spinner';
 })
 export class UserSectionComponent implements OnInit {
 
-  URL: string = "https://localhost:5001/api/v1/User/GetAllUser";
-  UTL_TOGET_BEARER_TOKEN: any = "https://localhost:5001/api/v1/TokenAuthentication/authentication";
+  users: any = [];
+  error: any;
   userCredentials:object = {
     userName: "nitingodase",
     password: "123qwe"
   };
-  bearerToken: any = '';
-  httpOptions: object = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      Authorization: ''
-    })
-  };
-  users: any = [];
-  error: any;
 
-  constructor(private HttpClient: HttpClient, private toastrService: ToastrService, private ngxSpinnerService: NgxSpinnerService) { }
+  constructor(private HttpClient: HttpClient, private userService:UsersService, private toastrService: ToastrService, private ngxSpinnerService: NgxSpinnerService) { }
 
   ngOnInit(): void {
     /*Promise.resolve(() => {
@@ -37,27 +29,22 @@ export class UserSectionComponent implements OnInit {
       return this.getAllUsers();
     });*/
     this.ngxSpinnerService.show();
-    this.getBearerToken();
+    this.getBearerToken(this.userCredentials);
     setTimeout(() => {
-      this.getAllUsers();
+       this.getAllUsers();
     }, 1000);
   }
-  getBearerToken() {
-    return this.HttpClient.post(this.UTL_TOGET_BEARER_TOKEN, this.userCredentials).subscribe((token) => {
-      //console.log(token)
-      //this.bearerToken = token;
-    }, (error) => {
-      //console.log(error);
-      this.bearerToken = this.handleError(error);
-    });
+
+  getBearerToken(Obj: any) {
+    this.userService.getBearerToken(Obj);
   }
+
   getAllUsers() {
     this.ngxSpinnerService.show();
-    this.HttpClient.get(this.URL, { headers: new HttpHeaders().set('Authorization', 'Bearer '+this.bearerToken) }).subscribe((responseData) => {
-      console.table(responseData);
-      this.users = responseData;
+    this.userService.getAllUsers().subscribe((usersDetails) => {
+      this.users = usersDetails;
     }, (error) => {
-      //console.log(error);
+      console.log(error);
       this.error = this.handleError(error);
     });
     setTimeout(() => {
@@ -66,20 +53,18 @@ export class UserSectionComponent implements OnInit {
   }
 
   private handleError(error: HttpErrorResponse) {
-    //console.log(error.error.text);
-    return error.error.text;
-    /*if (error.status === 0) {
+    if (error.status === 0) {
       // A client-side or network error occurred. Handle it accordingly.
-      return 'An error occurred:'+ error.error.text;
+      return 'An error occurred:'+ error.error;
     } else if (error.status === 401){
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong.
-      return `You are unauthorized :`+ error.error.text;
+      return `You are unauthorized :`+ error.error;
     }
     else {
       // Return an observable with a user-facing error message.
-      return error.error.text//throwError(() => new Error('Something bad happened; please try again later.'));
-    }*/
+      return throwError(() => new Error('Something bad happened; please try again later.'));
+    }
   }
 
 }
